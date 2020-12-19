23fefce02481da5b0e603fc5609237c690a2a200
Urvil Shah <urvil.shah@einfochips.com>
2020-12-19 20:20:25 +0530

    Add new functionlity of

--- listdir.groovy
+++ listdir.groovy
@@ -1,8 +1,11 @@
 import groovy.io.FileType
 
 // create the mono repo
+<<<<<<< Updated upstream
 // The man
 //The added
+=======
+>>>>>>> Stashed changes
 void mycommacode()
 {
     File fh2 = new File("fileContent.txt")
